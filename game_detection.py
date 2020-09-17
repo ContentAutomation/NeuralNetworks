2a60a78b1c1b61d39707e1b38fda23030e217018
Christian Coenen <coenen.christian@outlook.com>
2020-09-17 14:12:48 +0200

    Code is more generic now

--- game_detection.py
+++ game_detection.py
@@ -2,7 +2,9 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Input, LeakyReLU
 
-IMG_PATH = '/Users/christiancoenen/datasets/gameDetection'
+IMG_PATH = '/Users/christiancoenen/Google Drive/Social Media Automation/datasets/gameDetection'
+GAME_NAME = 'fortnite'
+EPOCHS = 3
 BATCH_SIZE = 16
 INPUT_SIZE = (224, 224)
 data_gen = tf.keras.preprocessing.image.ImageDataGenerator(validation_split=0.2)
@@ -11,7 +13,7 @@ train_generator = data_gen.flow_from_directory(
     directory=IMG_PATH,
     target_size=INPUT_SIZE,
     color_mode="rgb",
-    classes=None,
+    classes=[GAME_NAME, 'nogame'],
     class_mode="categorical",
     batch_size=BATCH_SIZE,
     shuffle=True,
@@ -27,7 +29,7 @@ validation_generator = data_gen.flow_from_directory(
     directory=IMG_PATH,
     target_size=INPUT_SIZE,
     color_mode="rgb",
-    classes=None,
+    classes=['fortnite', 'nogame'],
     class_mode="categorical",
     batch_size=BATCH_SIZE,
     shuffle=True,
@@ -49,11 +51,14 @@ for layer in base_model.layers:
 x = base_model.output
 x = tf.keras.layers.GlobalAveragePooling2D()(x)
 x = tf.keras.layers.Dropout(0.3)(x)
-x = tf.keras.layers.Dense(1024, activation=LeakyReLU(alpha=0.2))(x)
+x = tf.keras.layers.Dense(1024)(x)
+x = LeakyReLU(alpha=0.2)(x)
 x = tf.keras.layers.BatchNormalization()(x)
-x = tf.keras.layers.Dense(512, activation=LeakyReLU(alpha=0.2))(x)
+x = tf.keras.layers.Dense(512)(x)
+x = LeakyReLU(alpha=0.2)(x)
 x = tf.keras.layers.BatchNormalization()(x)
-x = tf.keras.layers.Dense(128, activation=LeakyReLU(alpha=0.2))(x)
+x = tf.keras.layers.Dense(128)(x)
+x = LeakyReLU(alpha=0.2)(x)
 x = tf.keras.layers.BatchNormalization()(x)
 predictions = tf.keras.layers.Dense(2, activation='softmax')(x)
 model = tf.keras.models.Model(inputs=base_model.input,  outputs=predictions)
@@ -65,6 +70,7 @@ model.fit(train_generator,
           steps_per_epoch=train_generator.samples // BATCH_SIZE,
           validation_data=validation_generator,
           validation_steps=validation_generator.samples // BATCH_SIZE,
-          epochs=5)
+          epochs=EPOCHS)
 
-# model.save(".")
+model.save(f"game_detection_{GAME_NAME}.h5")
+
