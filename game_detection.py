2ac4c0ed330e48f4922ba26137c041336553a475
Christian Coenen <coenen.christian@outlook.com>
2020-09-17 16:47:13 +0200

    Implemented GameDetection as class

--- game_detection.py
+++ game_detection.py
@@ -1,76 +1,129 @@
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Input, LeakyReLU
+from tensorflow.keras.layers import LeakyReLU
+from pathlib import Path
+import numpy as np
 
+SAVE_GENERATOR_IMAGES = False
+CONVERT_TO_GRAY = True
 IMG_PATH = '/Users/christiancoenen/Google Drive/Social Media Automation/datasets/gameDetection'
 GAME_NAME = 'fortnite'
 EPOCHS = 3
 BATCH_SIZE = 16
 INPUT_SIZE = (224, 224)
-data_gen = tf.keras.preprocessing.image.ImageDataGenerator(validation_split=0.2)
 
-train_generator = data_gen.flow_from_directory(
-    directory=IMG_PATH,
-    target_size=INPUT_SIZE,
-    color_mode="rgb",
-    classes=[GAME_NAME, 'nogame'],
-    class_mode="categorical",
-    batch_size=BATCH_SIZE,
-    shuffle=True,
-    seed=None,
-    save_to_dir=None,
-    save_prefix="",
-    save_format="png",
-    follow_links=False,
-    subset='training',
-    interpolation="nearest",
-)
-validation_generator = data_gen.flow_from_directory(
-    directory=IMG_PATH,
-    target_size=INPUT_SIZE,
-    color_mode="rgb",
-    classes=['fortnite', 'nogame'],
-    class_mode="categorical",
-    batch_size=BATCH_SIZE,
-    shuffle=True,
-    seed=None,
-    save_to_dir=None,
-    save_prefix="",
-    save_format="png",
-    follow_links=False,
-    subset='validation',
-    interpolation="nearest",
-)
 
-base_model = tf.keras.applications.ResNet50(include_top=False, weights="imagenet")
-# base_model = tf.keras.applications.VGG16(include_top=False, weights="imagenet")
-# base_model = tf.keras.applications.InceptionV3(include_top=False, weights="imagenet")
-for layer in base_model.layers:
-    layer.trainable = False
+class GameDetection:
 
-x = base_model.output
-x = tf.keras.layers.GlobalAveragePooling2D()(x)
-x = tf.keras.layers.Dropout(0.3)(x)
-x = tf.keras.layers.Dense(1024)(x)
-x = LeakyReLU(alpha=0.2)(x)
-x = tf.keras.layers.BatchNormalization()(x)
-x = tf.keras.layers.Dense(512)(x)
-x = LeakyReLU(alpha=0.2)(x)
-x = tf.keras.layers.BatchNormalization()(x)
-x = tf.keras.layers.Dense(128)(x)
-x = LeakyReLU(alpha=0.2)(x)
-x = tf.keras.layers.BatchNormalization()(x)
-predictions = tf.keras.layers.Dense(2, activation='softmax')(x)
-model = tf.keras.models.Model(inputs=base_model.input,  outputs=predictions)
+    def __init__(self,
+                 game_name,
+                 dataset_path,
+                 input_size,
+                 batch_size,
+                 save_generated_images=False,
+                 convert_to_gray=False
+                 ):
+        self.convert_to_gray = convert_to_gray
+        self.GENERATOR_IMAGES_FOLDER_NAME = 'gen'
+        Path(self.GENERATOR_IMAGES_FOLDER_NAME).mkdir(parents=True, exist_ok=True) if SAVE_GENERATOR_IMAGES else None
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        self.data_gen = tf.keras.preprocessing.image.ImageDataGenerator(
+            validation_split=0.2,
+            preprocessing_function=self.preprocessor
+        )
 
-# train the model
-model.fit(train_generator,
-          steps_per_epoch=train_generator.samples // BATCH_SIZE,
-          validation_data=validation_generator,
-          validation_steps=validation_generator.samples // BATCH_SIZE,
-          epochs=EPOCHS)
+        self.train_generator = self.data_gen.flow_from_directory(
+            directory=dataset_path,
+            target_size=input_size,
+            color_mode="rgb",
+            classes=[game_name, 'nogame'],
+            class_mode="categorical",
+            batch_size=batch_size,
+            shuffle=True,
+            seed=None,
+            save_to_dir=self.GENERATOR_IMAGES_FOLDER_NAME if save_generated_images else None,
+            save_prefix="",
+            save_format="png",
+            follow_links=False,
+            subset='training',
+            interpolation="nearest",
+        )
 
-model.save(f"game_detection_{GAME_NAME}.h5")
+        self.validation_generator = self.data_gen.flow_from_directory(
+            directory=dataset_path,
+            target_size=input_size,
+            color_mode="rgb",
+            classes=[game_name, 'nogame'],
+            class_mode="categorical",
+            batch_size=batch_size,
+            shuffle=True,
+            seed=None,
+            save_to_dir=None,
+            save_prefix="",
+            save_format="png",
+            follow_links=False,
+            subset='validation',
+            interpolation="nearest",
+        )
 
+        self.model = self.create_model()
+        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    def create_model(self):
+        base_model = tf.keras.applications.ResNet50(include_top=False, weights="imagenet")
+        # base_model = tf.keras.applications.VGG16(include_top=False, weights="imagenet")
+        # base_model = tf.keras.applications.InceptionV3(include_top=False, weights="imagenet")
+        for layer in base_model.layers:
+            layer.trainable = False
+
+        x = base_model.output
+        x = tf.keras.layers.GlobalAveragePooling2D()(x)
+        x = tf.keras.layers.Dropout(0.3)(x)
+        x = tf.keras.layers.Dense(1024)(x)
+        x = LeakyReLU(alpha=0.2)(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.Dense(512)(x)
+        x = LeakyReLU(alpha=0.2)(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.Dense(128)(x)
+        x = LeakyReLU(alpha=0.2)(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        predictions = tf.keras.layers.Dense(2, activation='softmax')(x)
+        model = tf.keras.models.Model(inputs=base_model.input, outputs=predictions)
+        return model
+
+    def train(self, epochs):
+        # train the model
+        self.model.fit(self.train_generator,
+                       steps_per_epoch=self.train_generator.samples // BATCH_SIZE,
+                       validation_data=self.validation_generator,
+                       validation_steps=self.validation_generator.samples // BATCH_SIZE,
+                       epochs=epochs)
+
+        self.model.save(f"game_detection_{GAME_NAME}.h5")
+
+    def preprocessor(self, image):
+        if self.convert_to_gray:
+            image = rgb2gray(image)
+            image = np.repeat(image[..., np.newaxis], 3, -1)
+        return image
+
+
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
+
+
+def preprocessor(image, convert_to_gray):
+    if convert_to_gray:
+        image = rgb2gray(image)
+        image = np.repeat(image[..., np.newaxis], 3, -1)
+    return image
+
+
+m = GameDetection(game_name='fortnite',
+                  dataset_path='/Users/christiancoenen/Google Drive/Social Media Automation/datasets/gameDetection',
+                  input_size=(224, 224),
+                  batch_size=16,
+                  save_generated_images=False,
+                  convert_to_gray=False
+                  )
+m.train(epochs=3)
